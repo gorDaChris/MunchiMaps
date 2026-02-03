f7b973417f4d26cc0a73f23c2ad6774171102da0
Chris <gorgorchrislo@gmail.com>
2026-02-03 16:43:36 -0500

    Verify that tbody is correct and clean-up section.

--- web_scrapper.py
+++ web_scrapper.py
@@ -15,22 +15,18 @@ driver = webdriver.Chrome(options=chrome_options)
 driver.get('https://publicsafety.rpi.edu/campus-security/card-access-schedule')
 
 # Wait until the table is present on the page
-wait = WebDriverWait(driver, 5, ignored_exceptions=(NoSuchElementException))
+wait = WebDriverWait(driver, 10, ignored_exceptions=(NoSuchElementException))
 wait.until(
     EC.visibility_of_element_located((By.XPATH, '//*[@id="block-paperclip-content"]/div/article/div/div/div/div/div/table'))
 )
 
 # Find the tbody element
 tbody = driver.find_element(By.XPATH, '//*[@id="block-paperclip-content"]/div/article/div/div/div/div/div/table/tbody')
-
 data = []
-
 # Iterate through all rows in the table body
 for tr in tbody.find_elements(By.XPATH, './/tr'):  # Use find_elements here to get all rows
     row = [item.text for item in tr.find_elements(By.XPATH, '//table/tbody/tr[1]/td[3]')]
     data.append(row)
-
-# //table/tbody/tr[1]/td[3]
 
 # TODO: Port this data to the front-end
 print(data)
